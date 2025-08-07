ed44e77fdf6ef7b6666fdba1c30b183c566efb66
gaochunxu <gaochunxu@bytedance.com>
2025-08-07 16:47:21 +0800

    update: mysql-init

--- release/deployment/docker-compose/bootstrap/mysql/healthcheck.sh
+++ release/deployment/docker-compose/bootstrap/mysql/healthcheck.sh
@@ -4,8 +4,13 @@ set -e
 
 export MYSQL_PWD="${COZE_LOOP_MYSQL_PASSWORD}"
 
-mysql -h 127.0.0.1 --protocol=TCP \
+if mysql -h 127.0.0.1 --protocol=TCP \
       -u "${COZE_LOOP_MYSQL_USER}" \
       --silent --skip-column-names \
       -e "SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='${COZE_LOOP_MYSQL_DATABASE}'" \
-      >/dev/null 2>&1
+      2>/dev/null \
+      | grep -q 1; then
+  exit 0
+else
+  exit 1
+fi
