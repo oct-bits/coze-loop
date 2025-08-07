e384786f1dd543ffe1362421c37b3306f6944976
gaochunxu <gaochunxu@bytedance.com>
2025-08-07 19:35:16 +0800

    update: nginx-init

--- release/deployment/helm-chart/charts/app/bootstrap/healthcheck.sh
+++ release/deployment/helm-chart/charts/app/bootstrap/healthcheck.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-wget -qO- http://localhost:8888/ping | grep -q pong
+if wget \
+    -qO- http://localhost:8888/ping \
+    2>/dev/null \
+    | grep -q pong; then
+  exit 0
+else
+  exit 1
+fi
--- release/deployment/helm-chart/charts/nginx/scripts/healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-curl -s "http://localhost:${COZELOOP_INGRESS_PORT_POD}" | grep -E -q 'cozeloop|nginx'
